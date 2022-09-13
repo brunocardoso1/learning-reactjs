74684ff8885633073b30ce171c87093ff3b563bb
Bruno Cardoso <bruno.alvescardoso@hotmail.com>
2022-09-12 23:41:52 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
 import './App.css';
 import {v4 as uuidv4} from 'uuid';
+import Header from './components/Header';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 const App = () => {
 
@@ -42,15 +44,34 @@ const App = () => {
      setTasks(newTask);
   }
 
+  const handleTaskDeletion = (taskId) => {
+    const newTasks = tasks.filter(task => task.id != taskId)
+    setTasks(newTasks)
+  }
+
 
   return (
-    <>
+    <Router>
       <div className="container">
-        <AddTask handleTaskAddition={handleTaskAddition}/>
-        <Tasks tasks={tasks} handleTaskClick={handleTaskClick}/>
+        <Header/>
+        <Route 
+          path="/" 
+          exact 
+          render={() => (
+          <>
+            <AddTask handleTaskAddition={handleTaskAddition}/>
+            <Tasks 
+              tasks={tasks} 
+              handleTaskClick={handleTaskClick}
+              handleTaskDeletion={handleTaskDeletion}
+              />
+          </>
+        )}/>
+        
+        
       </div>
    
-    </>
+    </Router>
   );
 };
 
